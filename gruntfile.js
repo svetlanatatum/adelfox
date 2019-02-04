bd671464016ab68a89953c0b8ecf40f871356b20
svetlanatatum <ambrikella@gmail.com>
2019-02-04 15:16:43 +0500

    Минифицирован index.html

--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,17 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        htmlmin: {
+            dist: {
+              options: {
+                removeComments: true,
+                collapseWhitespace: true
+              },
+              files: {
+                'index.html': 'index.html',
+              }
+            }
+        },
         sass: {
             dist: {
               files: {
@@ -22,6 +33,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
-    grunt.registerTask('default', ['sass', 'watch']);
+    grunt.registerTask('default', ['sass', 'htmlmin', 'watch']);
 };
