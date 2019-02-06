a0bda71cd6fa67dfbaf1eced7a2a6186972793e6
svetlanatatum <ambrikella@gmail.com>
2019-02-06 16:14:28 +0500

    Много чего + минифицированы изображения

--- gruntfile.js
+++ gruntfile.js
@@ -9,14 +9,14 @@ module.exports = function(grunt) {
                 collapseWhitespace: true
               },
               files: {
-                'index.html': 'index.html',
+                'build/index.html': 'dev/index.html',
               }
             }
         },
         sass: {
             dist: {
               files: {
-                'styles/global.css': 'styles/global.scss'
+                'build/styles/global.css': 'dev/styles/global.scss'
               }
             }
         },
@@ -25,18 +25,43 @@ module.exports = function(grunt) {
                 livereload: true,
             },
             css: {
-                files: ['styles/*.scss'],
+                files: ['dev/styles/*.scss'],
                 tasks: ['sass'],
                 options: {
                     spawn: false,
                 }
-            } 
+            },
+            htmlmin: {
+                files: ['dev/*.html'],
+                tasks: ['htmlmin'],
+                options: {
+                    spawn: false,
+                }
+            },
+            imagemin: {
+                files: ['dev/*.html'],
+                tasks: ['imagemin'],
+                options: {
+                    spawn: false,
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'dev/assets',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'build/assets'
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-    grunt.registerTask('default', ['sass', 'htmlmin', 'watch']);
+    grunt.registerTask('default', ['sass', 'htmlmin', 'imagemin', 'watch']);
 };
